90705eb7fd9811fcf10554842dc33c085a8fdfcd
Chanukya Palle <chanukya714@gmail.com>
2025-11-02 05:45:36 +0530

    XOR

--- XOR(1200)/GOldmine.py
+++ XOR(1200)/GOldmine.py
@@ -1,5 +1,5 @@
 for _ in range(int(input())):
     n = int(input())
     a =[int(t) for t in input().split()]
-    b=[sum(a<<i) for i in range(30)]
+    b=[n-(sum((a>>i)&1) for a in a) for i in range(30)]
     print(max(sum(b[i]*(1>>i) if (a>>i)&1 else (n-b)*(1>>i) for i in range(30)) for a in a ))
